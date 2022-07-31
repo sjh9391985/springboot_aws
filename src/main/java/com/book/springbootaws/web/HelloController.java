446d52e5d4cac39db2a8995c3184f342b54daf85
Son JaeHyeon <sjh9391985@gmail.com>
2022-07-31 17:30:13 +0900

    TEST CODE [Controller, Lombok]

--- src/main/java/com/book/springbootaws/web/HelloController.java
+++ src/main/java/com/book/springbootaws/web/HelloController.java
@@ -1,6 +1,8 @@
 package com.book.springbootaws.web;
 
+import com.book.springbootaws.web.dto.HelloResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /*
@@ -11,12 +13,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-    /*
-    * 2. GetMapping
-    * HTTP Method인 GET의 요청을 받을 수 있는 API를 만들어줌.
-    * */
+    /**
+     * 2. GetMapping
+     * HTTP Method인 GET의 요청을 받을 수 있는 API를 만들어줌.
+     *
+     * @RequestParam
+     * 외부에서 API로 넘긴 파라미터를 가져오는 어노테이션
+     * 여기서는 외부에서 name이란 이름으로 넘긴 파라미터를 메소드 파라미터 name에 저장하게 됨.
+     */
+
     @GetMapping("/hello")
     public String Hello(){
         return "hello";
+    }
+
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(@RequestParam("name") String name, @RequestParam("amount") int amount){
+        return new HelloResponseDto(name, amount);
     }
 }
--- src/test/java/com/book/springbootaws/web/HelloControllerTest.java
+++ src/test/java/com/book/springbootaws/web/HelloControllerTest.java
@@ -7,9 +7,9 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
+import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)  // 1
 @WebMvcTest(controllers = HelloController.class) // 2
@@ -26,37 +26,59 @@ public class HelloControllerTest {
                 .andExpect(status().isOk()) // 6
                 .andExpect(content().string(hello)); // 7
     }
+
+    @Test
+    public void lombok_test() throws Exception{
+        String name = "hello";
+        int amount = 1000;
+
+        mvc.perform(get("/hello/dto").param("name",name).param("amount", String.valueOf(amount))) // 8
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", is(name))) // 9
+                .andExpect(jsonPath("$.amount", is(amount)));
+    }
+
 }
 
-/*
-* 1. RunWith(SpringRunner.class)
-* 테스트 진행시 JUnit에 내장된 실행자 외에 다른 실행자를 실행시킴
-* 스프링부트 테스트와 JUnit 사이에 연결자 역할을 함.
-*
-* 2. WebMvcTest
-* 여러 스프링 테스트 어노테이션 중, Web에 집중할 수 있는 어노테이션
-* 선언시 @Controller, @ControllerAdvice 등을 사용가능
-* 단, @Service, @Component, @Repository 등은 사용불가
-*
-* 3. Autowired
-* 스프링이 관리하는 빈을 주입받음
-*
-* 4. private MockMvc mvc
-* 웹 API를 테스트 할 경우 사용
-* 스프링 MVC 테스트의 시작점
-* 이 클래스를 통해 HTTP GET, POST 등에 대한 API 테스트를 할 수 있음
-*
-* 5. mvc.perform(get("/hello"))
-* MockMvc를 통해 /hello 주소로 HTTP GET 요청을 함.
-* 체이닝이 지원되어 여러 검증기능을 선언가능.
-*
-* 6. .andExpect(status().isOk())
-* mvc.perform의 결과를 검증
-* HTTP Header의 Status를 검증가능
-* 200, 404, 500 등의 상태를 점검
-*
-* 7. .andExpect(content().string(hello))
-* mvc.perform의 결과를 검증
-* Controller에서 "hello"를 리턴하기에 이 값이 맞는지 검증.
-* */
+/**
+ * 1. RunWith(SpringRunner.class)
+ * 테스트 진행시 JUnit에 내장된 실행자 외에 다른 실행자를 실행시킴
+ * 스프링부트 테스트와 JUnit 사이에 연결자 역할을 함.
+ *
+ * 2. WebMvcTest
+ * 여러 스프링 테스트 어노테이션 중, Web에 집중할 수 있는 어노테이션
+ * 선언시 @Controller, @ControllerAdvice 등을 사용가능
+ * 단, @Service, @Component, @Repository 등은 사용불가
+ *
+ * 3. Autowired
+ * 스프링이 관리하는 빈을 주입받음
+ *
+ * 4. private MockMvc mvc
+ * 웹 API를 테스트 할 경우 사용
+ * 스프링 MVC 테스트의 시작점
+ * 이 클래스를 통해 HTTP GET, POST 등에 대한 API 테스트를 할 수 있음
+ *
+ * 5. mvc.perform(get("/hello"))
+ * MockMvc를 통해 /hello 주소로 HTTP GET 요청을 함.
+ * 체이닝이 지원되어 여러 검증기능을 선언가능.
+ *
+ * 6. .andExpect(status().isOk())
+ * mvc.perform의 결과를 검증
+ * HTTP Header의 Status를 검증가능
+ * 200, 404, 500 등의 상태를 점검
+ *
+ * 7. .andExpect(content().string(hello))
+ * mvc.perform의 결과를 검증
+ * Controller에서 "hello"를 리턴하기에 이 값이 맞는지 검증.
+ *
+ * 8. param
+ * API 테스트할 때 사용될 요청 파라미터를 설정
+ * 값은 String만 허용가능
+ * 숫자, 날짜 등의 데이터는 등록시 문자열로 변경해야함
+ *
+ * 9. jsonPath
+ * JSON 응답값을 필드별로 검증할 수 있는 메소드
+ * $를 기준으로 필드명을 명시
+ * name, amount를 검증하기에 $name, $amount로 검증
+ */
 
